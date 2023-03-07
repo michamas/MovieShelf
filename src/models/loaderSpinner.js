2a5375308b046d8698286fac279e62e8fe757e21
Hubert Dulas <111136742+Hubuuus@users.noreply.github.com>
2023-03-07 15:46:55 +0100

    backup fix

--- src/models/loaderSpinner.js
+++
@@ -1,6 +0,0 @@
-import { Loading } from "notiflix";
-
-export default function loader() {
-    // const color = getComputedStyle(document.documentElement).getPropertyValue(`$ic-accent`);
-    Loading.circle('Loading ...', {svgColor: '#ff6b08'});
-}
